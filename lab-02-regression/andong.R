90e9bc61125eabd2e376943f1212ef6c02236f98
Michael Ash <mash@econs.umass.edu>
2021-09-22 14:27:19 -0400

    Added Shih-Yen code

--- lab-02-regression/andong.R
+++ lab-02-regression/andong.R
@@ -1,6 +1,6 @@
 ## ZAP
 ## Based on Zhu, Ash, and Pollin (2006) https://doi.org/10.1080/0269217032000148645
-## Frisch Waugh Lovell partial regression model
+## Frisch Waugh Lovell partitioned regression model
 library(tidyverse)
 library(haven)
 andong <- read_dta("sbegnew.dta")
@@ -12,37 +12,62 @@ andong <-subset(andong, !is.na(gyp) & !is.na(tori))
 ## Challenge: rewrite with good tidyverse syntax
 attach(andong)
 
+frisch_waugh_lovell <- function(df, y, x, control) {
+    ## Frisch Waugh Lovell Partitioned Regression
+    ## Shih-Yen Pan & Michael Ash (2021)
+    ## df is the dataframe, y is the dependent variable, x is the key independent variable
+    ## control is a list of control variables
+    control <- (paste(control, collapse = " + ") )
 
-plot(tori,gyp)
-text(tori,gyp,name)
-ggplot(andong, aes(y=gyp, x=tori)) + geom_point() + geom_text(label=name) + geom_smooth(method="lm")
+    ## Bivariate regression for comparison
+    reg_bi <- as.formula(paste(y, " ~ ", x))
+    print("Bivariate Regression")
+    print(summary(lm_bi <- lm(reg_bi, df)))
 
-## Frisch Waugh Lovell (partial regression)
-fwl <- function(frame,gypu,toriu) {
-    name  <<- frame$name
-    lm.gypu <- lm(gyp ~ lrgdp + lsec + revcoup + govi + pii + bmpi + bpyi,frame)
-    gypu <<- lm.gypu$residual
-    lm.toriu <- lm(tori ~ lrgdp + lsec + revcoup + govi + pii + bmpi + bpyi,frame)
-    toriu <<- lm.toriu$residual
-    lm.u  <- lm(gypu ~ toriu)
-    summary(lm.u)
+    ## Multivariate regression for comparison
+    reg_mvr <- as.formula(paste(y, " ~ ", x, " + ", control))
+    print("Multivariate Regression")
+    print(summary(lm_mvr <- lm(reg_mvr, df)))
+
+    ## residualize y on the control variables
+    reg_ycontrol <- as.formula(paste(y, " ~ ", control))
+    u_y <- resid(lm(reg_ycontrol, df))
+
+    ## residualize x on the control variables
+    reg_xcontrol <- as.formula(paste(x, " ~ ", control))
+    u_x <- resid(lm(reg_xcontrol, df))
+
+    ## Frisch-Waugh-Lovell regression
+    lm_u <- lm(u_y ~ 0 + u_x)
+    print("Partitioned Regression")
+    print(summary(lm_u))
+    
+    plot(df[[x]],df[[y]], main="Scatterplot and bivariate regression", xlab=x, ylab=y)
+    abline(lm_bi)
+    dev.new()
+    plot(u_x,u_y, main="Residualized scatterplot and partitioned regression", xlab=paste("residualized", x ), ylab= paste("residualized", y))
+    abline(lm_u)
 }
 
-fwl(andong,gypu,toriu)
-plot(toriu,gypu)
-text(toriu,gypu,name)
+## list of control variables
+mycontrols <- c("lrgdp", "lsec", "revcoup", "govi", "pii", "bmpi", "bpyi")
+
+frisch_waugh_lovell(andong, "gyp", "tori", mycontrols)
 
 
-ggplot(data.frame(toriu,gypu,name), aes(x=toriu, y=gypu) ) + geom_point() + geom_text(label=name) + geom_smooth(method="lm")
 
 
 
+
+
+## ggplot(andong, aes(y=gyp, x=tori)) + geom_point() + geom_text(label=name) + geom_smooth(method="lm")
+## ggplot(data.frame(toriu,gypu,name), aes(x=toriu, y=gypu) ) + geom_point() + geom_text(label=name) + geom_smooth(method="lm")
 
 ## Not implemented: test omitting outliers
-plot(toriu,gypu)
-text(toriu,gypu,name)
-print("Left-click each country to omit; right-click when done.")
-omit <- identify(toriu,gypu,plot=FALSE)
-name[omit]
-except <- andong[-omit]
+## plot(toriu,gypu)
+## text(toriu,gypu,name)
+## print("Left-click each country to omit; right-click when done.")
+## omit <- identify(toriu,gypu,plot=FALSE)
+## name[omit]
+## except <- andong[-omit]
 
