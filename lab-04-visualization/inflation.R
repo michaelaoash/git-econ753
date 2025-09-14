7e52616cbe160971f749ff1d8e4283fa8d74121a
michaelaoash <mash@econs.umass.edu>
2025-09-14 12:31:44 -0400

    Added Core CPI Inflation and FT COTW

--- lab-04-visualization/inflation.R
+++ lab-04-visualization/inflation.R
@@ -1,33 +1,68 @@
 ## Annualized inflation over the previous 1, 3, 6, 12, 24, and 36 months
 library(tidyverse)
-options(dplyr.width=Inf, dplyr.print_max=Inf, scipen=10000)
+options(dplyr.width = Inf, dplyr.print_max = Inf, scipen = 10000)
 library(alfred)
+library(fredr)
 library(lubridate)
 library(reshape2)
 library(ggplot2)
 
-cpiaucsl <- get_fred_series("cpiaucsl")
+cpiaucsl <- get_fred_series("cpiaucsl")  ## CPI-U all items seasonally adjusted
 
+cpilfesl <- get_fred_series("cpilfesl")  ## CPI-U less fuel and food (i.e., "core") seasonally adjusted
+
+
+## fredr_set_key("2c43987fff98daa62e436c00e39f99ff")
+## params <- list(
+##     series_id = c("cpiaucsl", "cpilfesl"),
+##     frequency = c("m"),
+##     aggregation_method = c("avg"),  ## alternatives: eop, sum, avg
+##     observation_start = as.Date("1970-01-01")
+## )
+## inflation <- purrr::pmap(
+##                  .l = params,
+##                  .f = ~ fredr(series_id = ..1, frequency = ..2, aggregation_method = ..3, observation_start = ..4)) |>
+##     list_rbind()
+
+
+## All items
 inflation  <- cpiaucsl %>% mutate(
-                               i01 = (cpiaucsl/lag(cpiaucsl))^12 - 1,
-                               i03 = (cpiaucsl/lag(cpiaucsl,n=3))^4 - 1,
-                               i06 = (cpiaucsl/lag(cpiaucsl,n=6))^2 - 1,
-                               i12 = (cpiaucsl/lag(cpiaucsl,n=12)) - 1,
-                               i24 = (cpiaucsl/lag(cpiaucsl,n=24))^(1/2) - 1,
-                               i36 = (cpiaucsl/lag(cpiaucsl,n=36))^(1/3) - 1,
+                               i01 = (cpiaucsl / lag(cpiaucsl))^12 - 1,
+                               i03 = (cpiaucsl / lag(cpiaucsl, n = 3))^4 - 1,
+                               i06 = (cpiaucsl / lag(cpiaucsl, n = 6))^2 - 1,
+                               i12 = (cpiaucsl / lag(cpiaucsl, n = 12)) - 1,
+                               i24 = (cpiaucsl / lag(cpiaucsl, n = 24))^(1 / 2) - 1,
+                               i36 = (cpiaucsl / lag(cpiaucsl, n = 36))^(1 / 3) - 1,
                                date = date %m+% months(1)
                                )
 
-inflation  <- inflation %>% select(-cpiaucsl) %>% melt(id.vars="date")
+inflation  <- inflation %>% select(-cpiaucsl) %>% melt(id.vars = "date")
 
 
-inflation %>% filter(as.Date("2012-12-01")<=date) %>%
-    ggplot(aes(x=date, y=value, group=variable, color=variable)) + geom_line() +
-    scale_y_continuous(labels = scales::percent, breaks=c(-0.02,0,0.02,0.04,0.06,0.08)) +
-    scale_x_date(date_breaks = "1 year", date_minor_breaks="1 month")
+inflation %>%
+    filter(as.Date("2012-12-01") <= date) %>%
+    ggplot(aes(x = date, y = value, group = variable, color = variable)) + geom_line() +
+    scale_y_continuous(labels = scales::percent, breaks = c(-0.02, 0, 0.02, 0.04, 0.06, 0.08)) +
+    scale_x_date(date_breaks = "1 year", date_minor_breaks = "1 month")
 
 
 
-unrate <- get_fred_series("unrate")
+## Core
+inflation  <- cpilfesl %>% mutate(
+                               i01 = (cpilfesl / lag(cpilfesl))^12 - 1,
+                               i03 = (cpilfesl / lag(cpilfesl, n = 3))^4 - 1,
+                               i06 = (cpilfesl / lag(cpilfesl, n = 6))^2 - 1,
+                               i12 = (cpilfesl / lag(cpilfesl, n = 12)) - 1,
+                               i24 = (cpilfesl / lag(cpilfesl, n = 24))^(1 / 2) - 1,
+                               i36 = (cpilfesl / lag(cpilfesl, n = 36))^(1 / 3) - 1,
+                               date = date %m+% months(1)
+                               )
+
+inflation  <- inflation %>% select(-cpilfesl) %>% melt(id.vars = "date")
 
 
+inflation %>%
+    filter(as.Date("2012-12-01") <= date) %>%
+    ggplot(aes(x = date, y = value, group = variable, color = variable)) + geom_line() +
+    scale_y_continuous(labels = scales::percent, breaks = c(-0.02, 0, 0.02, 0.04, 0.06, 0.08)) +
+    scale_x_date(date_breaks = "1 year", date_minor_breaks = "1 month")
--- lab-04-visualization/unrate_bw.R
+++ lab-04-visualization/unrate_bw.R
@@ -23,23 +23,21 @@ fredr_set_key("2c43987fff98daa62e436c00e39f99ff")
 )
 nber.trim <- subset(nber, Start > as.Date("1970-01-01"))
 
-
 ## Unemployment rates U and 04 "not seasonally adjusted"
 ## Unemployment rates S and 14 "seasonally adjusted"
 ## 6 "black"
 ## 3 "white"
 
 params <- list(
-##    series_id = c("LNU04000006", "LNU04000003", "LNS14000006", "LNS14000003"),
-    series_id = c("LNS14000006", "LNS14000003"),
-    frequency = c("m"),
+    series_id = c("LNS14000006", "LNS14000003"),  ## alternative c("LNU04000006", "LNU04000003", "LNS14000006", "LNS14000003"),
+    frequency = c("q"),
+    aggregation_method = c("avg"),  ## alternatives: eop, sum
     observation_start = as.Date("1970-01-01")
 )
 bw <- purrr::pmap(
                  .l = params,
-                 .f = ~ fredr(series_id = ..1, frequency = ..2, observation_start = ..3)) |>
+                 .f = ~ fredr(series_id = ..1, frequency = ..2, aggregation_method = ..3, observation_start = ..4)) |>
     list_rbind()
-
 
 
 bw_wide <- bw %>%
@@ -53,14 +51,14 @@ bplot_triptych <- ggplot(melt(bw_df, id.vars = c("date"), variable.name = "race"
     facet_wrap(~ race, ncol = 1, scales = "free_y") +
     xlab("Year") +
     scale_x_date(date_breaks = "2 years", date_minor_breaks = "1 year", labels = date_format("%Y")) +
-    labs(title = "Ratio of Black:White Unemployment\nMonthly Data SA")
+    labs(title = "Ratio of Black:White Unemployment\nQuarterly Data SA")
 print(bplot_triptych + geom_rect(data = nber.trim, aes(xmin = Start, xmax = End, ymin = -Inf, ymax = +Inf), fill = "pink", alpha = 0.4))
 
 
 bplot <- ggplot(bw_df) +
     xlab("Year") +
     scale_x_date(date_breaks  =  "2 years", date_minor_breaks  =  "1 year", labels = date_format("%Y")) +
-    labs(title = "Ratio of Black:White Unemployment\nMonthly Data SA") + coord_cartesian(ylim = c(1, 3.5))
+    labs(title = "Ratio of Black:White Unemployment\nQuarterly Data SA") + coord_cartesian(ylim = c(1, 3.5))
 print(bplot +
       geom_line(aes(x = date, y = ratio)) +
       geom_rect(data = nber.trim, aes(xmin = Start, xmax = End, ymin = -Inf, ymax = +Inf), fill = "pink", alpha = 0.4))
@@ -70,7 +68,7 @@ print(bplot +
 bplot_bw <- ggplot(filter(melt(bw_df, id.vars = "date", variable.name = "race"), !(race %in% c("gap", "ratio")))) +
     geom_line(aes(x = date,  y = value,  group = race,  linetype = race)) +
     xlab("Year") + ylab("Unemployment") +
-    labs(title = "Black and White Unemployment\nMonthly Data SA") + coord_cartesian(ylim = c(0, 20)) + theme(legend.position = "top") +
+    labs(title = "Black and White Unemployment\nQuarterly Data SA") + coord_cartesian(ylim = c(0, 20)) + theme(legend.position = "top") +
     scale_x_date(date_breaks  =  "3 years",  date_minor_breaks  =  "1 year",  labels = date_format("%Y")) +
     geom_rect(data = nber.trim, aes(xmin = Start, xmax = End, ymin = -Inf,  ymax = +Inf), fill = "pink",  alpha = 0.4)
 print(bplot_bw)
@@ -78,7 +76,7 @@ print(bplot_bw)
 bplot_ratio <- ggplot(filter(melt(bw_df, id.vars = "date", variable.name = "race"), race == "ratio")) +
     geom_line(aes(x = date, y = value, group = race)) +
     xlab("Year") + ylab("Ratio") +
-    labs(title = "Black:White Unemployment Ratio\nMonthly Data SA") + coord_cartesian(ylim = c(1.5, 3)) +
+    labs(title = "Black:White Unemployment Ratio\nQuarterly Data SA") + coord_cartesian(ylim = c(1.5, 3)) +
     scale_x_date(date_breaks = "3 years", date_minor_breaks = "1 year", labels = date_format("%Y")) +
     geom_rect(data = nber.trim, aes(xmin = Start, xmax = End, ymin = -Inf, ymax = +Inf), fill = "pink", alpha = 0.4)
 print(bplot_ratio)
