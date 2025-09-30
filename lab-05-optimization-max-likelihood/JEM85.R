c7a2bd4eedd231d8a43c8e77b46b82c106b1211a
michaelaoash <mash@econs.umass.edu>
2025-09-30 14:53:37 -0400

    Used more strike data from Kennan

--- lab-05-optimization-max-likelihood/JEM85.R
+++ lab-05-optimization-max-likelihood/JEM85.R
@@ -1,31 +1,30 @@
-## Analyze strike duration data (Exercise from Greene)
+library(tidyverse)
+library(here)
 
-## Real industrial production
-rlip <- data.frame(year=1968:1976,
-                   ip=c(0.01138, 0.02299, -0.03957, -0.05467, 0.00535, 0.07427, 0.06450, -0.10443, -0.00700))
+## Analyze strike duration data (from Kennan, cited by Greene)
 
-## Strikes with durations
-df68 <- data.frame(year=1968, strike_duration=c(7, 9, 13, 14, 26, 29, 52, 130)                  )
-df69 <- data.frame(year=1969, strike_duration=c(9, 37, 41, 49, 52, 119)                         )
-df70 <- data.frame(year=1970, strike_duration=c(3, 17, 19, 28, 72, 99, 104, 114, 152, 153, 216) )
-df71 <- data.frame(year=1971, strike_duration=c(15, 61, 98)                                     )
-df72 <- data.frame(year=1972, strike_duration=c(2, 25, 85)                                      )
-df73 <- data.frame(year=1973, strike_duration=c(3, 10)                                          )
-df74 <- data.frame(year=1974, strike_duration=c(1,2,3,3,3,4,8,11,22,23,27,32,33,35,43,43,44,100))
-df75 <- data.frame(year=1975, strike_duration=c(5, 49)                                          )
-df76 <- data.frame(year=1976, strike_duration=c(2,12,12,21,21,27,38,42,117)                     )
+kennan_wide <- read_table(here("lab-05-optimization-max-likelihood", "JEM85.txt"),
+                          col_name = c("yrmo", "strikes", "IP",
+                                       "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "D11", "D12", "D13", "D14", "D15", "D16", "D17", "D18"),
+                          skip = 1
+                          )
 
-kiefer <- merge(rbind(df68, df69, df70, df71, df72, df73, df74, df75, df76),rlip)
-
+kennan <- kennan_wide %>%
+    pivot_longer(cols = starts_with("D"), values_to = "duration") %>%
+    filter(!is.na(duration)) %>%
+    mutate(
+        year = 1900 + yrmo %/% 100,
+        month = yrmo %% 100,
+        date = as.Date(paste0(as.character(year), "-", str_pad(as.character(month),2), "-01"), "%Y-%m-%d")
+        )
 
 ## One parameter Poisson (no covariates)
-
-with(kiefer, mean(strike_duration))
-with(kiefer, var(strike_duration)) ## really rejects poisson (mean != var)
-with(kiefer, log(mean(strike_duration)))
+with(kennan, mean(duration))
+with(kennan, var(duration)) ## really rejects poisson (mean != var)
+with(kennan, log(mean(duration)))
 
 ## Run using generalized linear model glm() function with packaged poisson
-summary(constant.glm <- glm(strike_duration ~ 1, data = kiefer, family = poisson))
+summary(constant.glm <- glm(duration ~ 1, data = kennan, family = poisson))
 logLik(constant.glm)
 predict(constant.glm, type = "response")
 
@@ -34,14 +33,13 @@ predict(constant.glm, type = "response")
 
 ## log(lambda) = X beta, in this case log(lambda) = beta We write the
 ## expression for the log likelihood (and must remember to set the optimizer to maximize)
-(y <- kiefer$strike_duration)
-(cons <- rep(1, length(kiefer$strike_duration)))
-(x <- kiefer$ip)
-(t <- kiefer$year - 1968)
-
+(y <- kennan$duration)
+(cons <- rep(1, length(kennan$duration)))
+(x <- kennan$IP)
+(t <- kennan$year - 1968)
 
 poissonll <- function(beta) {
-    sum( -exp(beta*cons)  +  y*beta*cons)
+    sum(-exp(beta*cons)  +  y*beta*cons)
 }
 
 ## Use optimizer to find beta, the log lambda that maximizes log likelihood  (try two different optimizers)
@@ -69,12 +67,12 @@ lnfac  <- function(yi) {
 
 ## Log likelihood function including the formal ln(Y!) stuff.  Needed to match the glm results for the value of the objective
 poissonll <- function(beta) {
-    sum(-exp(beta*cons) + y * beta * cons - sapply(y, lnfac))
+    sum(-exp(beta * cons) + y * beta * cons - sapply(y, lnfac))
 }
 
 ## Use optimizer to find beta, the log lambda that maximizes log likelihood  (try two different optimizers)
-(poissonll.opt  <- optimize(poissonll, interval=c(0,5), maximum=TRUE))
-(poissonll.opt  <- optim(par=1, poissonll, control=list(fnscale=-1)))
+(poissonll.opt  <- optimize(poissonll, interval = c(0, 5), maximum = TRUE))
+(poissonll.opt  <- optim(par = 1, poissonll, control = list(fnscale = -1)))
 ## Lambda
 exp(poissonll.opt$par)
 
@@ -90,29 +88,29 @@ exp(poissonll.opt$value)
 
 
 ## Two parameter Poisson (constant and one covariate)
-## log(lambda) = XB
-summary(kiefer2.glm <- glm(strike_duration ~ ip, data = kiefer, family = poisson))
+## log(lambda) = X beta
+summary(kennan2.glm <- glm(duration ~ IP, data = kennan, family = poisson))
 ## Recover the expected arrival rate in the bivarate case
-predict(kiefer2.glm,type="response")
+predict(kennan2.glm,type="response")
 ## Log likelihood function for two-parameter model
 poissonll2 <- function(beta) {
     sum(-exp(beta[1]*cons + beta[2]*x) + y*(beta[1]*cons + beta[2] * x))
 }
-(poissonll2.opt  <- optim(par=c(3,-7),  poissonll2, control=list(fnscale=-1), hessian=TRUE))
-
-rlip$predicted_duration = exp(poissonll2.opt$par[1] + poissonll2.opt$par[2]*rlip$ip)
-with(rlip, plot(year, predicted_duration))
-with(rlip, lines(year, predicted_duration))
-
+(poissonll2.opt  <- optim(par=c(3,-7),  poissonll2, control = list(fnscale=-1), hessian=TRUE))
 
 ## Beta_hat and s.e. beta_hat
 (poissonll2.opt$par)
 sqrt(diag(-solve(poissonll2.opt$hessian)))
 
 
+rlip <- kennan %>% group_by(yrmo) %>% summarize(IP = max(IP), date = max(date))
+rlip$predicted_duration = exp(poissonll2.opt$par[1] + poissonll2.opt$par[2]*rlip$IP)
+kennan %>% ggplot(aes(x = date, y = duration)) + geom_point() + geom_line(data = rlip, aes(x = date, y = predicted_duration))
+
+
 ## Three parameter Poisson (constant and two covariates)
 ## log(lambda) = XB
-summary(kiefer3.glm <- glm(strike_duration ~ ip + I(year-1968), data = kiefer, family = poisson))
+summary(kennan3.glm <- glm(duration ~ IP + I(year-1968) , data = kennan, family = poisson))
 ## Log likelihood function for three-parameter model
 poissonll3 <- function(beta) {
     sum(-exp(beta[1]*cons + beta[2]*x + beta[3]*t) + y * (beta[1]*cons + beta[2]*x + beta[3]*t))
---
+++ lab-05-optimization-max-likelihood/readme.txt
@@ -0,0 +1,13 @@
+Strike Duration Data
+
+See also
+https://search.r-project.org/CRAN/refmans/AER/html/StrikeDuration.html
+
+Greene source of the data
+https://pages.stern.nyu.edu/~wgreene/Text/tables/tablelist5.htm
+N.B. Greene data are only a (June) sample of the Kennan data
+
+https://fred.stlouisfed.org/series/IPMANSICN
+
+Greene, W.H. (2003). Econometric Analysis, 5th edition. Upper Saddle River, NJ: Prentice Hall.
+Kennan, J. (1985). The Duration of Contract Strikes in US Manufacturing. Journal of Econometrics, 28, 5–28.
